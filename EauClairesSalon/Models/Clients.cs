58a2bba63aef84c99eb0436f4258b8cf8a2289bb
basicjohn <edmondsonj@gmail.com>
2021-05-21 14:32:51 -0700

    WIP: gathering dropdown options

--- EauClairesSalon/Models/Clients.cs
+++ EauClairesSalon/Models/Clients.cs
@@ -1,14 +1,42 @@
+using System;
+
 namespace EauClairesSalon.Models
 {
   public class Client
   {
     public int ClientId { get; set; }
     public string Name { get; set; }
-    public string CurlinessType { get; set; }
-    public string StrandsType { get; set; }
-    public string VolumeType { get; set; }
+    public  string  CurlinessType { get;}
+    public  string  StrandsType { get; set;}
+    public  string VolumeType { get; set; }
     public int ContactNum { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
-  }
+
+
+      public enum CurlinessTypes : string
+    {
+      "1a: Stick-straight.",
+      "1b: Straight but with a slight body wave adding some volume.",
+      "1c: Straight with body wave and one or two visible S-waves.",
+      "2a: Loose with stretched S-waves throughout.",
+      "2b: Shorter with more distinct S-waves.",
+      "2c: Distinct S-waves, some spiral curling.",
+      "3a: Big, loose spiral curls.",
+      "3b: Bouncy ringlets.",
+      "3c: Tight corkscrews.",
+      "4a: Tightly coiled S-curls.",
+      "4b: Z-patterned",
+      "4c: Mostly Z-patterned"
+    }
+    StrandsType
+    {
+      "1a: Stick-straight.",
+      "1b: Straight but with a slight body wave adding some volume.",
+      "1c: Straight with body wave and one or two visible S-waves."
+    }
+
+
+
+}
 }
