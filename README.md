9627c279d42b7932379dd2332e44e876c9a90495
basicjohn <edmondsonj@gmail.com>
2021-05-24 23:47:32 -0700

    add database & update README

--- README.md
+++ README.md
@@ -40,34 +40,14 @@ _This project is created to be a vendor list app written in C# to take orders fo
    - Run the command `dotnet restore` to restore the dependencies that are listed in the .csproj
    - Finally, run the command `dotnet test` to run the tests!
 
-MySQL Install options (edit later)
-// THIS IS THE MOST IMPORTANT STEP, AND IT’S NEW. YOU MUST TELL US HOW TO SET UP YOUR DATABASE
+### Database Installation Instructions
 
-3. Create a database using SQL via MySQL:
-   ```
-   > CREATE DATABASE <firstname_lastname>;
-   > USE <firstname_lastname>;
-   > CREATE TABLE <tablename> (<column/property information>);
-   > CREATE TABLE <tablename> (<column/property information>);
-      ….(more if there are more!)
-   ```
-4. Create a database using SQL via MySQL: //TELL US HOW TO SET IT UP VIA THE MYSQL UI, WITH SPECIFIC TABLE NAMES, COLUMNS/PROPERTIES, AND THEIR TYPES!
-   Step one
-   Step two
-   Step 3
-   Step 4
-
-5. Create a database using SQL via MySQL: //INSTRUCT YOUR USER HOW TO USE THE PROVIDED FIRSTNAME_LASTNAME.SQL FILE WITH THEIR MYSQL
-   Steps….
-
-6. Run the application using:
-   ```
-   $ dotnet run
-   ```
-7. Visit the application via web browser:
-   ```
-   localhost:5000/
-   ```
+1. Open MySql WorkBench application
+2. On the Administration tab click 'Data Import/Restore' menu item
+3. Select radio button for 'Import from Self-Contained File'
+4. Click the two dots to select the export file title john_edmondson.sql dump file from this project root directory
+5. Click on 'New' and name the database schema john_edmondson
+6. Click 'Start Import' button
 
 ## Known Bugs
 
---
+++ john_edmondson.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE  IF NOT EXISTS `john_edmondson` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `john_edmondson`;
+-- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
+--
+-- Host: localhost    Database: john_edmondson
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (1,'Joan','Unknown','M: Medium','ii: Normal',1237849501,1),(2,'Susan','Unknown','C: Coarse','ii: Normal',1233487658,6),(3,'Frieda','Unknown','C: Coarse','ii: Normal',0,3);
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `stylists`
+--
+
+LOCK TABLES `stylists` WRITE;
+/*!40000 ALTER TABLE `stylists` DISABLE KEYS */;
+INSERT INTO `stylists` VALUES (1,'Stacy','2021-05-22 00:00:00',1234567890,'Sun-Wed'),(2,'Bill','2021-05-12 00:00:00',1234567889,'M-F'),(3,'Cynthia Clark','2021-06-02 00:00:00',0,'Sun-Thur'),(4,'Bobara Smith','2014-02-24 00:00:00',0,'Tues-Fri'),(5,'Bobara Smith','2014-02-24 00:00:00',0,'Tues-Fri'),(6,'Joe Pera','2021-05-22 00:00:00',0,'Tues-Sat');
+/*!40000 ALTER TABLE `stylists` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-24 23:46:26
