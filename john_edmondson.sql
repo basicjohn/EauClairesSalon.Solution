fbe4a07a9c1f069c46e2d165edcfb7645747185c
basicjohn <edmondsonj@gmail.com>
2021-05-26 10:18:20 -0700

    update database file

--- john_edmondson.sql
+++ john_edmondson.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `john_edmondson` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `john_edmondson`;
 -- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
 --
 -- Host: localhost    Database: john_edmondson
@@ -18,24 +16,42 @@ USE `john_edmondson`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `clients`
+-- Table structure for table `clients`
 --
 
-LOCK TABLES `clients` WRITE;
-/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
-INSERT INTO `clients` VALUES (1,'Joan','Unknown','M: Medium','ii: Normal',1237849501,1),(2,'Susan','Unknown','C: Coarse','ii: Normal',1233487658,6),(3,'Frieda','Unknown','C: Coarse','ii: Normal',0,3);
-/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `clients` (
+  `ClientId` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) NOT NULL,
+  `CurlinessType` varchar(45) DEFAULT 'Unknown',
+  `StrandsType` varchar(45) DEFAULT 'Unknown',
+  `VolumeType` varchar(45) DEFAULT 'Unknown',
+  `ContactNum` int(11) DEFAULT NULL,
+  `StylistId` int(11) NOT NULL COMMENT 'This is is a Foreign Key connected to the stylist table',
+  PRIMARY KEY (`ClientId`),
+  KEY `StylistId_idx` (`StylistId`),
+  CONSTRAINT `StylistId` FOREIGN KEY (`StylistId`) REFERENCES `stylists` (`StylistId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `stylists`
+-- Table structure for table `stylists`
 --
 
-LOCK TABLES `stylists` WRITE;
-/*!40000 ALTER TABLE `stylists` DISABLE KEYS */;
-INSERT INTO `stylists` VALUES (1,'Stacy','2021-05-22 00:00:00',1234567890,'Sun-Wed'),(2,'Bill','2021-05-12 00:00:00',1234567889,'M-F'),(3,'Cynthia Clark','2021-06-02 00:00:00',0,'Sun-Thur'),(4,'Bobara Smith','2014-02-24 00:00:00',0,'Tues-Fri'),(5,'Bobara Smith','2014-02-24 00:00:00',0,'Tues-Fri'),(6,'Joe Pera','2021-05-22 00:00:00',0,'Tues-Sat');
-/*!40000 ALTER TABLE `stylists` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `stylists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `stylists` (
+  `StylistId` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) DEFAULT NULL,
+  `EmploymentDate` datetime DEFAULT NULL,
+  `ContactNum` int(11) DEFAULT NULL,
+  `Schedule` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`StylistId`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -46,4 +62,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-24 23:46:26
+-- Dump completed on 2021-05-26 10:17:55
